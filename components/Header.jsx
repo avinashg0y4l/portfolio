b7e84554e8c0277baba9c53a5b7ea0856d896744
avinashg0y4l <avinashg0y4l@gmail.com>
2025-08-27 15:25:01 +0530

    No code changes made in Header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -47,8 +47,8 @@ const Header = () => {
             initial={{y: 30, opacity: 0}}
             whileInView={{y: 0, opacity: 1}}
             transition={{duration: 0.6, delay: 1.2}}
-            href="/Avinash_resume.pdf"
-            download="/Avinash_resume.pdf"
+            href="/Portfolio/Avinash_resume.pdf"
+            download="Avinash_resume.pdf"
             className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
                 my resume <Image src={assets.download_icon} alt='' className='w-4'/></motion.a>
         </div>
