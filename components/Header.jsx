207ac6efd9c33899835f8f63c0f5bee4457c9ca8
avinashg0y4l <avinashg0y4l@gmail.com>
2025-08-27 15:49:05 +0530

    Update resume link in Header component to point to the new URL

--- components/Header.jsx
+++ components/Header.jsx
@@ -47,7 +47,7 @@ const Header = () => {
             initial={{y: 30, opacity: 0}}
             whileInView={{y: 0, opacity: 1}}
             transition={{duration: 0.6, delay: 1.2}}
-            href="/portfolio/Avinash_resume.pdf"
+            href="https://raw.githubusercontent.com/avinashg0y4l/portfolio/main/public/Avinash_resume.pdf"
             download="Avinash_resume.pdf"
             className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
                 my resume <Image src={assets.download_icon} alt='' className='w-4'/></motion.a>
