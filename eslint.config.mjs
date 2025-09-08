f30759e53c34589918b31a58d095afe79448591d
avinashg0y4l <avinashg0y4l@gmail.com>
2025-09-08 23:53:01 +0530

    Enhance eslint configuration; add language options for improved parsing of JavaScript and TypeScript files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,16 @@ export default [
   },
   js.configs.recommended,
   {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
     plugins: {
       "@next/next": nextPlugin,
     },
